b27ffb034c3051e3da69a55cf5b49032709c87a5
Mykola Kyrylenko <bulletbest@gmail.com>
2022-01-09 18:27:04 +0200

    change config

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 //iTWBjRHOaLiCO5AC
 
 
-//mongodb+srv://Nikolay:iTWBjRHOaLiCO5AC@cluster0.tjzj0.mongodb.net/test
-
-const DB_HOST = "mongodb+srv://Nikolay:iTWBjRHOaLiCO5AC@cluster0.tjzj0.mongodb.net/online_shop?retryWrites=true&w=majority";
-
 const mongoose = require("mongoose");
+
+const {DB_HOST} = require("./config")
 
 mongoose.connect(DB_HOST)
     .then(() => console.log("Database connect"))
